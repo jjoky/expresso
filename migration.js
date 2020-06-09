99248ec6d5a8982fc876ba610d6d6866b955e063
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 01:58:06 -0400

    Create Employee Table

---
+++ migration.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+db.serialize(() => {
+    db.run(
+        `CREATE TABLE IF NOT EXISTS Employee (
+            'id' INTEGER NOT NULL,
+            'name' TEXT NOT NULL,
+            'position' TEXT NOT NULL,
+            'wage' INTEGER NOT NULL,
+            'is_current_employee' INTEGER DEFAULT 1,
+            PRIMARY KEY ('id')
+        )`
+    );
+});
