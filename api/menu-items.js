95f63e09034787592ec771b42f6255ef175f09e2
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 07:09:54 -0400

    PUT /api/menus/:menuId/menu-items/:menuItemId

--- api/menu-items.js
+++ api/menu-items.js
@@ -4,6 +4,22 @@ const menuItemsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemsRouter.param('menuItemId', (req,res, next, menuItemId) => {
+    db.get(
+        `SELECT * FROM MenuItem
+        WHERE MenuItem.id = ${req.params.menuItemId}`,
+        (error, row) => {
+            if (error) {
+                next(error);
+            } else if (row) {
+                next();
+            } else {
+                res.sendStatus(404);
+            }
+        }
+    );
+});
+
 menuItemsRouter.get('/', (req, res, next) => {
     db.all(
         `SELECT * FROM MenuItem
@@ -54,5 +70,50 @@ menuItemsRouter.post('/', (req, res, next) => {
         );
     }
 });
+
+menuItemsRouter.put('/:menuItemId', (req, res, next) => {
+    const name = req.body.menuItem.name;
+    const description = req.body.menuItem.description;
+    const inventory = req.body.menuItem.inventory;
+    const price = req.body.menuItem.price;
+    const menuId = req.params.menuId;
+    const menuItemId = req.params.menuItemId;
+
+    if (!name || !inventory || !price) {
+        res.sendStatus(400);
+    } else {
+        db.run(
+            `UPDATE MenuItem
+            SET name = $name,
+            description = $description,
+            inventory = $inventory,
+            price = $price,
+            menu_id = $menuId
+            WHERE MenuItem.id = $menuItemId`,
+            {
+                $name: name,
+                $description: description,
+                $inventory: inventory,
+                $price: price,
+                $menuId: menuId,
+                $menuItemId: menuItemId
+            },
+            function (error) {
+                if (error) {
+                    next(error);
+                } else {
+                    db.get(
+                        `SELECT * FROM MenuItem
+                        WHERE MenuItem.id = ${menuItemId}`,
+                        (error, row) => {
+                            res.json({menuItem: row});
+                        }
+                    );
+                }
+            }
+        );
+    }
+});
+
 
 module.exports = menuItemsRouter;
