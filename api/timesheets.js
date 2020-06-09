69bf69b5730defb072de74e4a8c2a4457ce2add6
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 04:21:49 -0400

    POST /api/employees/:employeeId/timesheets

--- api/timesheets.js
+++ api/timesheets.js
@@ -18,4 +18,39 @@ timesheetsRouter.get('/', (req, res, next) => {
     );
 });
 
+timesheetsRouter.post('/', (req, res, next) => {
+    const hours = req.body.timesheet.hours;
+    const rate = req.body.timesheet.rate;
+    const date = req.body.timesheet.date;
+    const employeeId = req.params.employeeId;
+
+    if (!hours || !rate || !date) {
+        res.sendStatus(400);
+    } else {
+        db.run(
+            `INSERT INTO Timesheet (hours, rate, date, employee_id)
+            VALUES ($hours, $rate, $date, $employee_id)`,
+            {
+                $hours: hours,
+                $rate: rate,
+                $date: date,
+                $employee_id: employeeId
+            },
+            function(error) {
+                if (error) {
+                    next(error);
+                } else {
+                    db.get(
+                        `SELECT * FROM Timesheet
+                        WHERE Timesheet.id = ${this.lastID}`,
+                        (error, row) => {
+                            res.status(201).json({timesheet: row});
+                        }
+                    );
+                }
+            }
+        );
+    }
+});
+
 module.exports = timesheetsRouter;
