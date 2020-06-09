9e2ac00081b85de212586dc2f85cb61c45e22bdd
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 02:53:16 -0400

    POST /api/employees

--- api/employees.js
+++ api/employees.js
@@ -4,6 +4,23 @@ const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+employeesRouter.param('employeeId', (req, res, next, employeeId) =>{
+    db.get(
+        `SELECT * FROM Employee
+        WHERE id = ${employeeId}`,
+        (error, row) => {
+            if (error) {
+                next(error);
+            } else if (row) {
+                req.employee = row;
+                next();
+            } else {
+                res.sendStatus(404);
+            }
+        }
+    );
+});
+
 employeesRouter.get('/', (req, res, next) => {
     db.all(
         `SELECT * FROM Employee WHERE is_current_employee = 1`,
@@ -15,6 +32,49 @@ employeesRouter.get('/', (req, res, next) => {
             }
         }
     );
-})
+});
+
+employeesRouter.get('/:employeeId', (req, res, next) => {
+    res.status(200).json({employee: req.employee})
+});
+
+employeesRouter.post('/', (req, res, next) => {
+    const name = req.body.employee.name;
+    const position = req.body.employee.position;
+    const wage = req.body.employee.wage;
+    const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+
+    if (!name || !position || !wage) {
+        res.sendStatus(400);
+    } else {
+        db.run(
+            `INSERT INTO Employee (name, position, wage, is_current_employee)
+            VALUES ($name, $position, $wage, $isCurrentEmployee)`,
+            {
+                $name: name,
+                $position: position,
+                $wage: wage,
+                $isCurrentEmployee: isCurrentEmployee
+            },
+            function(error) {
+                if (error) {
+                    next(error);
+                } else {
+                    db.get(
+                        `SELECT * FROM Employee
+                        WHERE Employee.id = ${this.lastID}`,
+                        (error, row) => {
+                            if (error) {
+                                next(error);
+                            } else {
+                                res.status(201).json({employee: row})
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    }
+});
 
 module.exports = employeesRouter;
