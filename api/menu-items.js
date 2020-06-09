bc1b146d396c7bdcea5f41e40b5a3d55bee6b7cb
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 06:23:17 -0400

    GET /api/menus/:menuId/menu-items

--- api/menu-items.js
+++ api/menu-items.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const menuItemsRouter = express.Router({mergeParams: true});
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+menuItemsRouter.get('/', (req, res, next) => {
+    db.all(
+        `SELECT * FROM MenuItem
+        WHERE MenuItem.menu_id = ${req.params.menuId}`,
+        (error, rows) => {
+            if (error) {
+                next(error);
+            } else {
+                res.json({menuItems: rows});
+            }
+        }
+    );
+});
+
 
 
 module.exports = menuItemsRouter;
