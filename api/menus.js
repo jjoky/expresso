e51eb041c5e946b7271e46642f8d7171b68db72c
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 05:11:46 -0400

    GET /api/menus

--- api/menus.js
+++ api/menus.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const menusRouter = express.Router();
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menusRouter.get('/', (req, res, next) => {
+    db.all(
+        `SELECT * FROM Menu`,
+        (error, rows) => {
+            if (error) {
+                next(error);
+            } else {
+                res.status(200).json({menus: rows});
+            }
+        }
+    );
+});
 
 module.exports = menusRouter;
