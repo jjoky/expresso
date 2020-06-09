3cbc6aa9edd001cd1101820451ca73a2647486ff
Jacques Joky <jacques.joky@gmail.com>
2020-06-09 04:03:18 -0400

    GET /api/employees/:employeeId/timesheets

--- api/timesheets.js
+++ api/timesheets.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const timesheetsRouter = express.Router({mergeParams: true});
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+timesheetsRouter.get('/', (req, res, next) => {
+    db.all(
+        `SELECT * FROM Timesheet
+        WHERE Timesheet.employee_id = ${req.params.employeeId}`,
+        (error, rows) => {
+            if (error) {
+                next(error);
+            } else {
+                res.status(200).json({timesheets: rows});
+            }
+        }
+    );
+});
 
 module.exports = timesheetsRouter;
